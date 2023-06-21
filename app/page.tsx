c1170ff0ca39ccd993057eb5420e90a4518a1c26
James Jordan <jajordan@blackberry.com>
2023-06-21 14:54:21 -0400

    Changed some text and replaced cv link with mail link temporarily

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="lg:w-2/5 lg:top-0 lg:sticky lg:max-h-screen lg:p-24 p-4">
         <h1 className="bg-o-background w-fit text-6xl md:mb-2 font-medium max-sm:tracking-tight max-sm:text-6xl">James Jordan</h1>
         <h2 className="bg-o-background w-fit text-2xl mb-10 max-sm:mb-4 font-light">Web Developer at BlackBerry</h2>
-        <p className="bg-o-background w-fit text-lg leading-tight antialiased tracking-wider mb-12 font-extralight">Software Engineer seeking Winter 2023 internships.</p>
+        <p className="bg-o-background w-fit text-lg leading-tight antialiased tracking-wider mb-12 font-extralight">Software Engineer seeking Winter 2024 internships.</p>
         <ScrollNav />
         <ul className="max-w-xs mt-12 flex gap-x-2 md:gap-x-4 lg:gap-x-10" aria-label="Social Links">
           <li className="w-12 h-12" aria-label="Linkedin link">
@@ -22,9 +22,14 @@ export default function Home() {
               <Image className="bg-o-background transition ease-in-out duration-150 hover:scale-125" src="images/github.svg" width={32} height={32} alt="GitHub" />
             </a>
           </li>
-          <li className="w-12 h-12" aria-label="CV link">
+          {/* <li className="w-12 h-12" aria-label="CV link">
             <a className="block" href="#UpdateThis" target="_blank">
               <Image className="bg-o-background transition ease-in-out duration-150 hover:scale-125" src="images/cv.svg" width={32} height={32} alt="CV" />
+            </a>
+          </li> */}
+          <li className="w-12 h-12" aria-label="Email link">
+            <a className="block" href="mailto: jamesjordan2027@gmail.com">
+              <Image className="bg-o-background transition ease-in-out duration-150 hover:scale-125" src="images/email.svg" width={32} height={32} alt="email" />
             </a>
           </li>
         </ul>
@@ -33,16 +38,16 @@ export default function Home() {
         <section className="lg:mt-4 lg:p-8 pl-2 pr-2 pt-0 mt-0 relative mb-16 max-lg:pb-4 bg-o-background lg:border-2.5 rounded-xl border-section-outline shadow-xl">
           <h2 className="text-3xl mb-4">About</h2>
           <p className="text-xl font-thin leading-tight antialiased tracking-wider mb-4">
-            Ever since learning about MIT&#39;s Scratch in my elementary school&#39;s computer lab, I&#39;ve been tinkering
+            Ever since learning MIT&#39;s Scratch programming language in my elementary school&#39;s computer lab, I&#39;ve been experimenting
             with new technologies to build cool projects I&#39;m excited about. I&#39;m currently studying
             Computer Science at the University of Waterloo.
           </p>
           <p className="text-xl font-thin leading-tight antialiased tracking-wider mb-4">
             Thus far, I&#39;ve been a fullstack engineer and co-founder
-            of <a className="font-normal hover:text-link-highlighted" href="https://evoke-app.com">Evoke</a>, a freelance web developer for a local tutoring business, a web developer at <a href="https://blackberry.com/" className="font-normal hover:text-link-highlighted">BlackBerry</a>.
+            of <a className="font-normal hover:text-link-highlighted" href="https://evoke-app.com" target="_blank">Evoke</a>, a freelance web developer for a local tutoring business, and a web developer at <a href="https://blackberry.com/" className="font-normal hover:text-link-highlighted" target="_blank">BlackBerry</a>.
             In addition to my work experience, I&#39;ve worked on several projects about stuff I&#39;m passionate about, such as
-            an <a href="https://github.com/jamesj64/Automated-Theorem-Prover" className="font-normal hover:text-link-highlighted">Automated Theorem Prover</a>, several games in the Unity Game Engine (here&#39;s my favorite: <a href="#" className="font-normal hover:text-link-highlighted">title</a>),
-            and a <a href="https://github.com/jamesj64/goose2" className="font-normal hover:text-link-highlighted">remote control goose powered by a Raspberry Pi.</a>
+            an <a href="https://github.com/jamesj64/Automated-Theorem-Prover" className="font-normal hover:text-link-highlighted" target="_blank">Automated Theorem Prover</a>, several games in the Unity Game Engine (<a href="https://jamesj63.itch.io/coldblooded" className="font-normal hover:text-link-highlighted" target="_blank">Coldblooded</a> is my favorite),
+            and a <a href="https://github.com/jamesj64/goose2" className="font-normal hover:text-link-highlighted" target="_blank">remote control goose powered by a Raspberry Pi.</a>
           </p>
           <p className="text-xl font-thin leading-tight antialiased tracking-wider">
             When I&#39;m not programming, you might find me reading about the philosophy of language or philosophical logic,
@@ -70,8 +75,7 @@ export default function Home() {
               application. Designed and developed REST API with Node.js and Express to handle payment processing with Stripe’s API, validate user requests with
               Firebase JWT’s and authenticate and charge clients for requests to AI models hosted on AWS EKS
               cluster. Implemented a Firebase NoSQL Realtime Database with security rules for storing
-              essential user information, including cryptographically secure authentication tokens generated
-              by the REST API. Configured GitHub Actions to establish CI/CD pipeline for building and
+              essential user information. Configured GitHub Actions to establish CI/CD pipeline for building and
               deploying the web app and Node.js server. Performed unit testing with Postman, used cloud
               services such as Google Cloud/Firebase, DigitalOcean."
               skills={["React", "Node.js", "Express.js", "TypeScript", "DevOps", "AWS", "GCP", "Firebase"]}
@@ -93,7 +97,7 @@ export default function Home() {
               endDate="Present"
               title="Hack & Slash"
               skills={[ "C#", "OOP", "Unity Game Engine" ]}
-              description="Developed hack and slash game for WebGL and Desktop platforms in the Unity Game Engine."
+              description="Developing hack and slash game for WebGL and Desktop platforms in the Unity Game Engine. More to come soon..."
             />
             <ExperienceCard
               startDate="Nov 2021"
