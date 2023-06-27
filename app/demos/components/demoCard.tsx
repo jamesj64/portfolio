ced0479e944c1b82b5b495134087166568ddc831
James Jordan <jajordan@blackberry.com>
2023-06-27 15:28:17 -0400

    Updated demo cards

--- app/demos/components/demoCard.tsx
+++ app/demos/components/demoCard.tsx
@@ -1,12 +1,29 @@
 import Link from "next/link";
+import Image from "next/image";
 
-export default function DemoCard() {
+interface DemoCard {
+  title: string,
+  description: string,
+  url: string,
+  internalLink?: boolean,
+  previewImg: string
+}
+
+export default function DemoCard(props: DemoCard) {
   return (
-    <li className="relative w-full grid md:grid-cols-8">
-      <p className="sm:col-span-2">Hello</p>
-      <div className="sm:col-span-6">
-        <h2 className="text-xl">Demo</h2>
-        <p className="text-md">Description description description description description description <Link href="/demos/hack-slash">click me</Link> description description</p>
+    <li className="relative w-full md:w-5/12 lg:w-3/12 grid md:grid-cols-8 justify-around group content-center">
+      {props.url && (
+        props.internalLink ?
+          <Link href={props.url} aria-label={props.title} target="_blank" className="w-full h-full absolute top-0 left-0 z-20" />
+          : <a href={props.url} aria-label={props.title} target="_blank" className="w-full h-full absolute top-0 left-0 z-20" />
+      )}
+      <div className="absolute w-full h-full -inset-x-3 -inset-y-2 z-0 hidden rounded-md transition motion-reduce:transition-none md:block md:group-hover:bg-experience-background md:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] md:group-hover:drop-shadow-lg"/>
+      <p className="z-10 sm:col-span-2 hidden md:block">
+        <Image src={props.previewImg} alt={props.title} width={100} height={100}/>
+      </p>
+      <div className="z-10 sm:col-span-6 p-4 pt-0">
+        <h2 className="text-xl group-hover:text-link-highlighted">{props.title}</h2>
+        <p className="text-md text-center">{props.description}</p>
       </div>
     </li>
   );
--- app/demos/page.tsx
+++ app/demos/page.tsx
@@ -5,16 +5,45 @@ export default function Page() {
   return (
     <main className="min-h-screen text-center items-center p-12">
       <h1 className="text-4xl">Demos</h1>
-      <ul className="mt-16 md:grid grid-cols-3 gap-y-8 w-full h-full justify-between">
-        <DemoCard/>
-        <DemoCard/>
-        <DemoCard/>
-        <DemoCard/>
-        <DemoCard/>
-        <DemoCard/>
-        <DemoCard/>
+      <ul className="mt-16 md:flex gap-y-8 w-full h-full flex-wrap justify-center gap-x-12">
+        <DemoCard
+          title="Demo"
+          description="description description description description description description description description description"
+          url="/demos/hack-slash"
+          internalLink={true}
+          previewImg="/images/github.svg"
+        />
+        <DemoCard
+          title="Demo"
+          description="description description description description description description description description description"
+          url="/demos/hack-slash"
+          internalLink={true}
+          previewImg="/images/github.svg"
+        />
+        <DemoCard
+          title="Demo"
+          description="description description description description description description description description description"
+          url="/demos/hack-slash"
+          internalLink={true}
+          previewImg="/images/github.svg"
+        />
+        <DemoCard
+          title="Demo"
+          description="description description description description description description description description description"
+          url="/demos/hack-slash"
+          internalLink={true}
+          previewImg="/images/github.svg"
+        />
+        <DemoCard
+          title="Demo"
+          description="description description description description description description description description description"
+          url="/demos/hack-slash"
+          internalLink={true}
+          previewImg="/images/github.svg"
+        />
       </ul>
-      <BackgroundEffect/>
+      {/* Uncomment below for merge */}
+      {/* <BackgroundEffect/> */}
     </main>
   );
 };
