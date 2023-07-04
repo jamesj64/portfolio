50a37b5de2f25da83aa4f9c004e35044e1bf1892
James <jamesjordan2027@gmail.com>
2023-07-04 19:55:06 -0400

    Added fullscreen to game on click and fixed internal links in experience card component to not open in new tab

--- app/demos/hack-slash/page.tsx
+++ app/demos/hack-slash/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react";
+import React, { useRef } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function Page() {
@@ -11,16 +11,18 @@ export default function Page() {
     codeUrl: "hack-slash/build/Web.wasm.br"
   });
 
+  const game = useRef<HTMLCanvasElement>(null);
+
   const loadingPercentage = Math.round(loadingProgression * 100);
 
   return (
-    <main className="min-h-screen text-center items-center p-12">
-      {isLoaded === false && (
-        <div className="text-4xl mt-48">
-          <p>Loading... ({loadingPercentage}%)</p>
+    <main className="flex min-h-screen justify-center overflow-hidden">
+      <div className="w-11/12 max-h-screen p-24 text-center pt-4">
+        <p className="text-4xl pb-4">{isLoaded ? "Hack & Slash" : `Loading... (${loadingPercentage}%)`}</p>
+        <div onClick={() => { if (game.current && isLoaded) game.current.requestFullscreen(); }}>
+          <Unity className="w-full h-full" unityProvider={unityProvider} ref={game}/>
         </div>
-      )}
-      <Unity className="w-full h-full" unityProvider={unityProvider} />
+      </div>
     </main>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import ScrollNav from "./components/scrollNav";
 import BackgroundEffect from "./components/backgroundEffect";
 import ExperienceCard from "./components/experienceCard";
-import Link from "next/link";
 import Advert from "./components/advert";
 
 export default function Home() {
