3a110cf8804e174fa57f1df256d3c73e2e9ec75d
James <jamesjordan2027@gmail.com>
2023-07-05 21:46:31 -0400

    Fixed internalLink prop for DemoCard component

--- app/demos/components/demoCard.tsx
+++ app/demos/components/demoCard.tsx
@@ -14,7 +14,7 @@ export default function DemoCard(props: DemoCard) {
     <li className="relative w-full md:w-5/12 xl:w-3/12 grid md:grid-cols-8 justify-around group content-center w-min:w-max">
       {props.url && (
         props.internalLink ?
-          <Link href={props.url} aria-label={props.title} target="_blank" className="w-full h-full absolute top-0 left-0 z-20" />
+          <Link href={props.url} aria-label={props.title} className="w-full h-full absolute top-0 left-0 z-20" />
           : <a href={props.url} aria-label={props.title} target="_blank" className="w-full h-full absolute top-0 left-0 z-20" />
       )}
       <div className="absolute w-full h-full z-0 hidden rounded-md transition motion-reduce:transition-none md:block md:group-hover:bg-experience-background md:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] md:group-hover:drop-shadow-lg"/>
